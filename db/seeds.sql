5aec62a50811f1c40732a4e3f1d702657ec52669
Dana Cronin <cronin43224235@gmail.com>
2019-12-21 11:44:32 -0500

    filling database with 3 burgers

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Munchy');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('Hangover Cure');
+INSERT INTO `burgers_db`.`burgers` (`burger_name`) VALUES ('CanYouFitItInYourFace');
