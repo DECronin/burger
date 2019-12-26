7eead1e756f53eea85a4cad7bf9e1b7f3c74219d
Dana Cronin <cronin43224235@gmail.com>
2019-12-26 17:00:06 -0500

    external functions and orm.insert

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,27 @@
 const connection = require("../config/connection.js");
 
+function printQuestionMarks(num) {
+    var arr = [];
+    for (var i = 0; i < num; i++) {
+      arr.push("?");
+    }
+    return arr.toString();
+}
+
+function objToSql(ob) {
+    var arr = [];
+    for (var key in ob) {
+      var value = ob[key];
+      if (Object.hasOwnProperty.call(ob, key)) {
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        arr.push(key + "=" + value);
+      }
+    }
+    return arr.toString();
+}
+
 const orm = {
     selectAll: function(tableInput, cb){
         let queryString = "SELECT * FROM " + tableInput + ";";
@@ -8,8 +30,17 @@ const orm = {
           cb(result);
         });
     },
-    insertOne: function(){
-
+    insertOne: function(table, cols, vals, cb){
+        var queryString = `INSERT INTO ${table} (${cols.toString()}) VALUES(${printQuestionMarks(vals.length)})`;
+        console.log(queryString);
+    
+        connection.query(queryString, vals, function(err, result) {
+            if (err) {
+            throw err;
+            }
+    
+            cb(result);
+        });
     },
     updateOne: function(){
 
