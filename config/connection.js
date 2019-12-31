21ef946cec72160487e74441345e8b7349f0ab07
Dana Cronin <cronin43224235@gmail.com>
2019-12-31 01:14:12 -0700

    psudocoding connection in anticipation of heroku

--- config/connection.js
+++ config/connection.js
@@ -1,4 +1,8 @@
 const mysql = require("mysql");
+
+// if (process.env.JAWSDB_URL) {
+//   connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
 const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
@@ -6,6 +10,7 @@ const connection = mysql.createConnection({
   password: "root",
   database: "burgers_db"
 });
+// };
 connection.connect(function(err) {
   if (err) {
     console.error("error connecting: " + err.stack);
