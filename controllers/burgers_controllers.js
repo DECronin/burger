346c7c7672bbab05c239b67a8d1f7f167f8d8ec0
Dana Cronin <cronin43224235@gmail.com>
2019-12-31 00:40:31 -0700

    post and put functions

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -1,7 +1,39 @@
 const express = require("express");
 const router = express.Router();
-const burgers = require("../models/burger.js")
+const burger = require("../models/burger.js")
 
+router.get("/", function(req, res) {
+    burger.selectAll(function(data) {
+      var thisObject = {
+        burgers: data
+      };
+      console.log(thisObject);
+      res.render("index", thisObject);
+    });
+});
 
+router.post("/api/burgers", function(req, res) {
+    burger.insertOne([
+      "burger_name"
+    ], [
+      req.body.name
+    ], function(result) {
+      res.json({ id: result.insertId });
+    });
+});
+
+router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
+    console.log("condition", condition);
+    burger.updateOne({
+      devoured: req.body.devoured
+    }, condition, function(result) {
+      if (result.changedRows == 0) {
+        return res.status(404).end();
+      } else {
+        res.status(200).end();
+      }
+    });
+  })
 
 module.exports = router;
