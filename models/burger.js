9c4601ae603e2ce5bbd04f9408781b0a730475df
Dana Cronin <cronin43224235@gmail.com>
2019-12-30 23:57:06 -0700

    insert and update added

--- models/burger.js
+++ models/burger.js
@@ -6,6 +6,16 @@ let burger = {
             cb(res);
         });
     },
+    insertOne: function(cols, vals, cb) {
+        orm.insertOne("burgers", cols, vals, function(res) {
+          cb(res);
+        });
+    },
+    updateOne: function(objColVals, condition, cb) {
+        orm.updateOne("burgers", objColVals, condition, function(res) {
+          cb(res);
+        });
+    }
 };
 
 module.exports = burger;
