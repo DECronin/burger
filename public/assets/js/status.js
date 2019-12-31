ad424f51c221a2b6a067ba700a4c7fbb7398e06a
Dana Cronin <cronin43224235@gmail.com>
2019-12-31 02:01:45 -0700

    removing reset

--- public/assets/js/status.js
+++ public/assets/js/status.js
@@ -2,21 +2,6 @@ $(document).ready(() => {
     $(".consume").on("click", (event) => {
         event.preventDefault();
         let url = "/api/burgers/" + $(event.currentTarget).data("id");
-        sendAjax(url);
-    });
-
-    $("#reset-all").on("click", (event) => {
-        event.preventDefault();
-        // let apiLength = 
-        console.log("Reset All Burger Statuses to Devoured: false");
-        // for(i = 0; i < ; i++){
-        //     let url = "/api/burgers/" + i;
-        //     sendAjax(url)
-        // }
-
-    })
-
-    function sendAjax(url){
         console.log(url);
         $.ajax({
             url: url,
