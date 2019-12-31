b2adbea5c1a8765bb23888b88ff3124bb303bd78
Dana Cronin <cronin43224235@gmail.com>
2019-12-31 01:55:10 -0700

    testing ability to change all statuses back to false

--- public/assets/js/status.js
+++ public/assets/js/status.js
@@ -1,6 +1,22 @@
 $(document).ready(() => {
     $(".consume").on("click", (event) => {
-        let url = "/api/burger/" + $(event.currentTarget).data("id");
+        event.preventDefault();
+        let url = "/api/burgers/" + $(event.currentTarget).data("id");
+        sendAjax(url);
+    });
+
+    $("#reset-all").on("click", (event) => {
+        event.preventDefault();
+        // let apiLength = 
+        console.log("Reset All Burger Statuses to Devoured: false");
+        // for(i = 0; i < ; i++){
+        //     let url = "/api/burgers/" + i;
+        //     sendAjax(url)
+        // }
+
+    })
+
+    function sendAjax(url){
         console.log(url);
         $.ajax({
             url: url,
@@ -8,5 +24,5 @@ $(document).ready(() => {
         }).then(() => {
             location.reload();
         });
-    });
+    }
 });
