4078c9685ca8216b5640e35f104fb4d6ca5389b6
Dana Cronin <cronin43224235@gmail.com>
2019-12-31 01:54:15 -0700

    accomodating change in passed function variables

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -25,9 +25,7 @@ router.post("/api/burgers", function(req, res) {
 router.put("/api/burgers/:id", function(req, res) {
     var condition = "id = " + req.params.id;
     console.log("condition", condition);
-    burger.updateOne({
-      devoured: req.body.devoured
-    }, condition, function(result) {
+    burger.updateOne('devoured', 'true', condition, function(result) {
       if (result.changedRows == 0) {
         return res.status(404).end();
       } else {
